5830d7f575d16be28192ad150759f1f3efcc1314
Jerry <jamesharold1738@gmail.com>
2023-01-08 17:16:31 -0500

    fixed API loops

--- assets/script.js
+++ assets/script.js
@@ -7,35 +7,80 @@ var p = document.querySelector("p")
 var temperature = document.querySelector(".Temperature")
 var humidity = document.querySelector(".humidity")
 //var formSubmitHandler = function (event) {
-    // event.preventDefault();
-    // var city = searchBar
-    // if (city) {
-    //     findCity(city)
+// event.preventDefault();
+// var city = searchBar
+// if (city) {
+//     findCity(city)
 
-    // } else {
-    //     alert("please enter a city/country")
-    // }
+// } else {
+//     alert("please enter a city/country")
+
+function fetch5day(lat, lon) {
+    var API = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=08553de1a0124c7f4f47f18b48ab8cf6"
+    fetch(API).then(response => response.json()).then(data => {
+        console.log(data)
+        for(i = 0; i < data.list.length; i += 8) {
+            var div = document.createElement("div");
+            document.getElementById("forecast").append(div)
+            var tempP = document.createElement("p")
+            tempP.textContent = "temperature: " + data.list[i].main.temp + "F"
+            var humidity = document.createElement("p");
+            humidity.textContent = "Humidity: " + data.list[i].main.humidity + "%"
+            var Wind = document.createElement("p");
+            Wind.textContent = "wind speed: " + data.list[i].wind.speed + "MPH"
+            var Date = document.createElement("p");
+            Date.textContent = dayjs.unix(data.list[i].dt).format("M/D/YYYY");
+            div.append(Date, tempP, humidity, Wind)
+        }
+    })
+
+}
+
 
 
 function findCity(event) {
     event.preventDefault()
-    var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchBar.value + "&appid=08553de1a0124c7f4f47f18b48ab8cf6"
+    var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchBar.value + "&units=imperial&appid=08553de1a0124c7f4f47f18b48ab8cf6"
     fetch(cityUrl).then(response =>
-     response.json()).then(data => {var displayCity = data['name'];
-     var displayTemperature = data['main']['temp']
-     var displayHumidity = data['main']['humidity']
+        response.json()).then(data => {
+            var displayCity = data['name'];
+            var displayTemperature = data['main']['temp']
+            var displayHumidity = data['main']['humidity']
 
-     p.innerHTML = displayCity;
-     temperature.innerHTML =  "Temperature: " + displayTemperature;
-     
-    humidity.innerHTML =  "Humidity: " + displayHumidity
-    })}
+            p.innerHTML = displayCity;
+            temperature.innerHTML = "Temperature: " + displayTemperature;
+            var Date = document.createElement("p")
+            document.querySelector("#currentDate").textContent = dayjs.unix(data.dt).format("M/D/YYYY");
+
+
+            humidity.innerHTML = "Humidity: " + displayHumidity
+            fetch5day(data.coord.lat, data.coord.lon);
+
+        })
+}
 
 function test(event) {
     event.preventDefault()
     var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchBar.value + "&appid=08553de1a0124c7f4f47f18b48ab8cf6"
     fetch(cityUrl).then(response =>
-     response.json()).then(data => console.log(data))}
+        response.json()).then(data => {
+            console.log(data)
+            // for (i = 0; i < data.list.length; i += 8) {
+            //     var div = document.createElement("div");
+            //     document.getElementById("forecast").append(div)
+            //     var tempP = document.createElement("p")
+            //     tempP.textContent = data.list[i].main.temp
+            //     var humidity = document.createElement("p");
+            //     humidity.textContent = data.list[i].main.humidity
+            //     var Wind = document.createElement("p");
+            //     Wind.textContent = data.list[i].wind.speed
+            //     var Date = document.createElement("P");
+            //     date.textContent = dayjs.unix(data.list[i].dt).format("M/D/YYYY");
+            //     div.append(Date, tempP, humidity, Wind)
+            // }
+        })
+
+}
 
 
 
